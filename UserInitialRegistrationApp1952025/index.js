f0bbb3dd92f605adc23fa2c1da19fd41f44b3f07
Ashutosh Das <Ashutosh.Das@Ltts.com>
2025-06-04 17:58:49 +0530

    Registration

---
+++ UserInitialRegistrationApp1952025/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import  RegisterScreen from './RegisterScreen';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => RegisterScreen);
