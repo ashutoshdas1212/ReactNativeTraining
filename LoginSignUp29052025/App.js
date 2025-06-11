8f1d8802719a89c4e6d321a196f8459c0853c903
Ashutosh Das <Ashutosh.Das@Ltts.com>
2025-06-11 10:38:07 +0530

    Profile Screen Input Field

--- LoginSignUp29052025/App.js
+++ LoginSignUp29052025/App.js
@@ -1,70 +1,8 @@
-
-// import * as React from 'react';
-// import {View, Text} from 'react-native';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import Home from './src/Home';
-// import Login from './src/Login';
-// import SignUp from './src/SignUp';
-// import {useNavigation} from '@react-navigation/native';
-
-// const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
-
-// function MainTabs() {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Login" component={Login} />
-//       <Tab.Screen name="SignUp" component={SignUp} />
-//     </Tab.Navigator>
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name="Home"
-//           component={Home}
-//           options={{
-//             title: 'Home Screen',
-//             headerStyle: {
-//               backgroundColor: 'green',
-//             },
-//             headerTintColor: '#fff',
-//             headerTitleStyle: {
-//               fontWeight: 'bold',
-//             },
-//             headerTitleAlign: 'center'
-//           }}
-//         />
-//         <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
-//         <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}} />
-        
-//  <Stack.Screen name="MainTabs" component={MainTabs} options={{headerShown:false}} />
-
-//       </Stack.Navigator>
-      
-//       </NavigationContainer>
-      
-//         )
-//         }
-
-
-
-
-
-
-
-
-
 import * as React from 'react';
-import {View, Text,Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Home from './src/Home';
 import Login from './src/Login';
@@ -75,68 +13,93 @@ import Details from './src/Details';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function HomeStack()
-{
-  return(
- <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: 'Home Screen',
-            headerStyle: {
-              backgroundColor: 'green',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerTitleAlign: 'center'
-          }}
-        />
-        <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
-        <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}} />
-        
-
-
-      </Stack.Navigator>
+function HomeStack() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Home Screen',
+          headerStyle: {
+            backgroundColor: 'green',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
   );
 }
 export default function App() {
   return (
-    <View style={styles.container}> 
-    <NavigationContainer>
-     <Tab.Navigator screenOptions={{tabBarStyle:90}}>
-      <Tab.Screen name="HomeStack" component={HomeStack} options={{headerShown:false,title:'Home',tabBarIcon:({size,color})=>{
-        return(
-          <Image style={{width:size,height:size,tintColor:color}} source={require('./images/Home.png')} />
-        )
-      }}}></Tab.Screen>
-       <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon:({size})=>{
-        return(
-          <Image style={{width:size,height:size}} source={require('./images/Profile.jpg')} />
-        )
-      }}} />
-        <Tab.Screen name="Details" component={Details} options={{tabBarIcon:({size,color})=>{
-        return(
-          <Image style={{width:size,height:size,tintColor:color}} source={require('./images/Details.png')} />
-        )
-      }}} />
-     </Tab.Navigator>
-
-      
-    </NavigationContainer>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{tabBarStyle: 90}}>
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              headerShown: false,
+              title: 'Home',
+              tabBarIcon: ({size, color}) => {
+                return (
+                  <Image
+                    style={{width: size, height: size, tintColor: color}}
+                    source={require('./images/Home.png')}
+                  />
+                );
+              },
+            }}></Tab.Screen>
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              tabBarIcon: ({size}) => {
+                return (
+                  <Image
+                    style={{width: size, height: size}}
+                    source={require('./images/Profile.jpg')}
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Details"
+            component={Details}
+            options={{
+              tabBarIcon: ({size, color}) => {
+                return (
+                  <Image
+                    style={{width: size, height: size, tintColor: color}}
+                    source={require('./images/Details.png')}
+                  />
+                );
+              },
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-
-  container:{
-    flex:1,
-   
-  }
-
-  })
-  
+  container: {
+    flex: 1,
+  },
+});
